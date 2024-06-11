071348934cd57ef1c948ac5cc8a3e9129937334c
Franchesco <fbramirezc.g@gmail.com>
2024-06-11 13:24:53 -0500

    Create S3 bucket paths

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,5 +1,15 @@
 import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
-  name: 'petcarefiles'
+  name: 'petcarefiles',
+  access: (allow) => ({
+    'pet-profile-pictures/{entity_id}/*': [
+      allow.guest.to(['read']),
+      allow.entity('identity').to(['read', 'write', 'delete'])
+    ],
+    'picture-submissions/*': [
+      allow.authenticated.to(['read', 'write']),
+      allow.guest.to(['read', 'write'])
+    ],
+  })
 });
