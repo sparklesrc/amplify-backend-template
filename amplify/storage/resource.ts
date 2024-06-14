e9e512aa63566bcbdb0ebee61ee0e68224e9b0bf
Franchesco <fbramirezc.g@gmail.com>
2024-06-14 12:36:50 -0500

    Update S3 path

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -3,7 +3,7 @@ import { defineStorage } from '@aws-amplify/backend';
 export const storage = defineStorage({
   name: 'petcarefiles',
   access: (allow) => ({
-    '{entity_id}/*': [
+    'owners/{entity_id}/*': [
       allow.guest.to(['read']),
       allow.entity('identity').to(['read', 'write', 'delete'])
     ]
