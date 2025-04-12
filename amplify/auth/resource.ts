e5d67ac1dbee3741af91b694da0fa1110b45b1a1
SparKLes <fbramirezc@gmail.com>
2025-04-12 18:27:11 -0500

    Override Auth

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -4,4 +4,30 @@ export const auth = defineAuth({
   loginWith: {
     email: true,
   },
+  userAttributes: {
+    email: {
+      required: true,
+      mutable: false,
+    },
+    givenName: {
+      required: false,
+      mutable: true,
+    },
+    familyName: {
+      required: false,
+      mutable: true,
+    },
+    birthdate: {
+      required: false,
+      mutable: true,
+    },
+    phoneNumber: {
+      required: false,
+      mutable: true,
+    },
+    address: {
+      required: false,
+      mutable: true,
+    },
+  }
 });
