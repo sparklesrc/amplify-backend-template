e9a5202bfbef43ae7c7c84850ae2d387c5894712
SparKLes <fbramirezc@gmail.com>
2025-04-12 13:06:58 -0500

    Override Auth

---
+++ amplify/auth/override.ts
@@ -0,0 +1,55 @@
+export const override = (stack: any) => {
+    const userPool = stack.resources.cognitoUserPool;
+  
+    userPool.schema = [
+      ...(userPool.schema ?? []),
+      {
+        name: 'custom:first_name',
+        attributeDataType: 'String',
+        mutable: true,
+        required: false,
+      },
+      {
+        name: 'custom:last_name',
+        attributeDataType: 'String',
+        mutable: true,
+        required: false,
+      },
+      {
+        name: 'custom:birth_date',
+        attributeDataType: 'String',
+        mutable: true,
+        required: false,
+      },
+      {
+        name: 'custom:phone_number',
+        attributeDataType: 'String',
+        mutable: true,
+        required: false,
+      },
+      {
+        name: 'custom:departamento',
+        attributeDataType: 'String',
+        mutable: true,
+        required: false,
+      },
+      {
+        name: 'custom:ciudad',
+        attributeDataType: 'String',
+        mutable: true,
+        required: false,
+      },
+      {
+        name: 'custom:distrito',
+        attributeDataType: 'String',
+        mutable: true,
+        required: false,
+      },
+      {
+        name: 'custom:address',
+        attributeDataType: 'String',
+        mutable: true,
+        required: false,
+      },
+    ];
+  };
