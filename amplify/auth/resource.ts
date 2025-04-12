406dd8305510b0c0ab142e7d22e2ca5ba92d87c5
SparKLes <fbramirezc@gmail.com>
2025-04-12 15:00:14 -0500

    Override Auth

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,22 +1,7 @@
 import { defineAuth } from '@aws-amplify/backend';
-import parameters from './parameters.json';
 
-interface Parameters {
-  requiredAttributes: string[];
-}
-
-const typedParameters: Parameters = parameters as unknown as Parameters;
-
-/**
- * Define and configure your auth resource
-  userAttributes: typedParameters.requiredAttributes.reduce((acc, attr) => {
- */
 export const auth = defineAuth({
   loginWith: {
     email: true,
   },
-  userAttributes: typedParameters.requiredAttributes.reduce((acc: Record<string, { required: boolean }>, attr) => {
-    acc[attr] = { required: true };
-    return acc;
-  }, {} as Record<string, { required: boolean }>),
 });
