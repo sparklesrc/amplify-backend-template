a0b1039d6e05322ba2747e1bfe3aa08a52ef268b
SparKLes <fbramirezc@gmail.com>
2025-04-21 19:21:10 -0500

    Include Required Attributes for SignUp

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -5,7 +5,11 @@ export const auth = defineAuth({
   loginWith: {
     email: true,
   },
-  signUpAttributes: {
-    required: ['name', 'family_name', 'phone_number', 'birthdate', 'address'],
+  userAttributes: {
+    given_name: true,
+    family_name: true,
+    birthdate: true,
+    address: true,
+    phone_number: true,
   }
 });
